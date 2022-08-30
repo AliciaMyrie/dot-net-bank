ec051b6ecac4f7fc978e5621230a89a016d13861
Alicia Myrie <aliciamyrie@gmail.com>
2022-08-30 11:43:13 -0400

    bank of boca code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// test bank program
+Console.WriteLine("Welcome to Bank Boca Code");
+
+BankAccount account = new BankAccount();
+account.AccountNumber  = "12345";
+
+account.MakeDeposit(100);
+
+BankAccount account2 = new SavingsAccount(500);
+
+try{
+    decimal newBalance = account2.MakeDepositGetBalance(-1);
+} catch ( Exception ex) {
+    Console.WriteLine($"we got an error when running MakeDepositGetBalance(-1)"+ex.Message);
+}
+
+
+
+Console.WriteLine($"The balance of account is {account.Balance}");
+Console.WriteLine($"The balance of account is {account.AccountNumber}");
+Console.WriteLine($"The balance of account is {account2.Balance}");
+
+
+
